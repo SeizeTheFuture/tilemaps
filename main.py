3ab78139a6aab27b4547488cf515302e84011927
Matt Gottilla <matthew.gottilla@gmail.com>
2022-03-20 16:37:42 -0700

    Added player movement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 
+#Use 2D vectors
+vector = pygame.math.Vector2
+
 # Initialize pygame
 pygame.init()
 
@@ -37,17 +40,69 @@ class Tile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
 
+class Player(pygame.sprite.Sprite):
+    """A Player class the user can control"""
+
+    def __init__(self, x, y, grass_tiles, water_tiles):
+        super().__init__()
+        self.image = pygame.image.load("knight.png")
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft= (x,y)
+
+        self.grass_tiles = grass_tiles
+        self.water_tiles = water_tiles
+
+        #Kinematics vectors (first value is x, second value is y)
+        self.position = vector(x, y)
+        self.velocity = vector(0, 0)
+        self.acceleration = vector(0, 0)
+
+        #Kinematic constants
+        self.HORIZONTAL_ACCELERATION = 2
+        self.HORIZONTAL_FRICTION = 0.15
+        self.VERTICAL_ACCELERATION = 0.5 #Gravity
+
+    def update(self):
+        #Set the acceleration vector to (0,0) so there is initially no acceleration
+        #If there are no key presses then horizontal acceleration should be 0, gravity will always be present
+        self.acceleration = vector(0, self.VERTICAL_ACCELERATION)
+
+        #If the user is pressing a left or right key, set the x-component of the acceleration vector to a non-zero value
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.acceleration.x = -1 * self.HORIZONTAL_ACCELERATION
+        if keys[pygame.K_RIGHT]:
+            self.acceleration.x = self.HORIZONTAL_ACCELERATION
+
+        #Calculate new kinematics values
+        self.acceleration.x -= self.velocity.x * self.HORIZONTAL_FRICTION
+        self.velocity += self.acceleration
+        self.position += self.velocity + .5*self.acceleration
+
+        #Update rect based on kinematics calculations
+        self.rect.bottomleft  =  self.position
+
+        #Check for collisions with grass tiles
+        collided_platforms = pygame.sprite.spritecollide(self, self.grass_tiles, False)
+        if collided_platforms:
+            self.position.y = collided_platforms[0].rect.top + 1
+            self.velocity.y = 0
+
+        #Check for collisions with water tiles
+        if pygame.sprite.spritecollide(self, self.water_tiles, False):
+            print("You can't swim!")
 
 # Create sprite groups
 main_tile_group = pygame.sprite.Group()
 grass_tile_group = pygame.sprite.Group()
 water_tile_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
 
-# Create the tile map: 0 -> no tile, 1 -> dirt, 2 -> grass, 3 -> water
+# Create the tile map: 0 -> no tile, 1 -> dirt, 2 -> grass, 3 -> water, 4 -> player
 # 20 rows and 30 columns
 tile_map = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -79,6 +134,9 @@ for i in range(len(tile_map)):
             Tile(j * 32, i * 32, 2, main_tile_group, grass_tile_group)
         elif tile_map[i][j] == 3:
             Tile(j * 32, i * 32, 3, main_tile_group, water_tile_group)
+        elif tile_map[i][j] == 4:
+            player = Player(j*32, i*32 + 32, grass_tile_group, water_tile_group)
+            player_group.add(player)
 
 # Load in a background image
 background_image = pygame.image.load("background.png")
@@ -98,6 +156,11 @@ while running:
 
     # Draw tiles
     main_tile_group.draw(display_surface)
+
+    #Update and draw sprites
+    player_group.update()
+    player_group.draw(display_surface)
+
     # Update the display and tick the clock
     pygame.display.update()
     clock.tick(FPS)
